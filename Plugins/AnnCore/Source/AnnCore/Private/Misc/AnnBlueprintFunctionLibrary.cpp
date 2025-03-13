32ff29e1f23edd9032e72b86d08b6fc23112f792
HUSTJJD <1173703591@qq.com>
2025-03-13 22:31:33 +0800

    clang format

--- Plugins/AnnCore/Source/AnnCore/Private/Misc/AnnBlueprintFunctionLibrary.cpp
+++ Plugins/AnnCore/Source/AnnCore/Private/Misc/AnnBlueprintFunctionLibrary.cpp
@@ -1,11 +1,12 @@
 
 
-
 #include "Misc/AnnBlueprintFunctionLibrary.h"
 
-void UAnnBlueprintFunctionLibrary::GetFilesInDir(const FString& InPath, TArray<FString>& OutFiles)
+void UAnnBlueprintFunctionLibrary::FindFiles(const FString& InPath, const FString& Extension, const bool bRecursively, TArray<FString>& OutFiles)
 {
-
+	IPlatformFile& platformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (bRecursively)
+		platformFile.FindFilesRecursively(OutFiles, *InPath, *Extension);
+	else
+		platformFile.FindFiles(OutFiles, *InPath, *Extension);
 }
-
-
--- Plugins/AnnCore/Source/AnnCore/Public/Misc/AnnBlueprintFunctionLibrary.h
+++ Plugins/AnnCore/Source/AnnCore/Public/Misc/AnnBlueprintFunctionLibrary.h
@@ -2,25 +2,21 @@
 
 #pragma once
 
+#include "AnnBlueprintFunctionLibrary.generated.h"
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
-#include "AnnBlueprintFunctionLibrary.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class ANNCORE_API UAnnBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
-# pragma region file system
+
+#pragma region file system
 	UFUNCTION(BlueprintCallable)
-	static void GetFilesInDir(const FString& InPath, TArray<FString>& OutFiles);
+	static void FindFiles(const FString& InPath, const FString& Extension, const bool bRecursively, TArray<FString>& OutFiles);
 
-
-
-# pragma endregion
-	
-	
+#pragma endregion
 };
